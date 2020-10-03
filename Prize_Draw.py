19d30e2d1419340dbe0ed302eb451fea16d8c2ed
Dezoway <kostya.trepalin.2013@mail.ru>
2020-10-03 17:25:05 +0300

    Remastered Kata

--- Prize_Draw.py
+++ Prize_Draw.py
@@ -1,27 +1,27 @@
-def rank(st,we,n):
-    if n > len(we):
-        return "Not enough participants"
-    elif len(st) == 0:
-        return "No participants"
-    else:
+from functools import cmp_to_key
+def rank(st, we, n):
+    if(not st): return "No participants"
+    st = st.split(',')
+    if(n > len(st)): return  "Not enough participants"
+    pairs = [(name,word(name.lower())*we[i]) for i, name in enumerate(st)]
+    pairs = sorted(pairs,key=cmp_to_key(compare))
+    return pairs[n-1][0]
+def word(x): return len(x)+sum(ord(chr) - ord('a') + 1 for chr in x)
+def compare(x,y):
+    if x[1] == y[1]:
+        if x[0] == y[0]: return 0
+        if x[0] > y[0]: return 1
+        return -1
+    return y[1] - x[1]
 
-        Alphavite="abcdefghijklmnopqrstuvwxyz"                      #these are sketches
-        st1=st.split(",")
-        som=0
-        som1=[]
-        som2=[]
-        for x in st1:
-            for z in x.lower():
-                som+=Alphavite.index(z)+1
-            som1.append((som+len(x))*we[st1.index(x)])
-            som2.append((som+len(x))*we[st1.index(x)])
-            som1.append(x)
-            som=0
-        som2.sort(reverse=True)
-        num=som2[n-1]
-        return(som1[som1.index(num)+1])
+
+
+
+
+
 
 
 print(rank("Addison,Jayden,Sofia,Michael,Andrew,Lily,Benjamin", [4, 2, 1, 4, 3, 1, 2], 4))
+print(rank("Addison,Jayden,Sofia,Michael,Andrew,Lily,Benjamin", [7, 2, 2, 6, 3, 1, 2], 4))
 print(rank("Lagon,Lily", [1, 5], 2))
 print(rank("", [4, 2, 1, 4, 3, 1, 2], 4))
